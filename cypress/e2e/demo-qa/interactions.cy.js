be814a7308aec79abf6b30ae1ed3be1b824b6b26
SmirnovaAleksa <smirnovaaleksa39@gmail.com>
2022-07-12 15:25:48 +0300

    Interactions done

--- cypress/e2e/demo-qa/interactions.cy.js
+++ cypress/e2e/demo-qa/interactions.cy.js
@@ -10,30 +10,28 @@ context("Interactions", () => {
     // Create scenario 1:
     it("scenario 1", () => {
       // Click on "Cras justo odio"
-      cy.get('#verticalListContainer > :nth-child(1)').click();
+      SelectablePage.row1.click();
       // Validate that class contains active
-      cy.get('#verticalListContainer > :nth-child(1)').should("have.class","active");
+      SelectablePage.row1.should("have.class","active");
       // Click on "Dapibus ac facilisis in"
-      cy.get('#verticalListContainer > :nth-child(2)').click();
+      SelectablePage.row2.click();
       // Validate that chosen buttons contains class active
-      cy.get('#verticalListContainer > :nth-child(2)').should("have.class","active");
+      SelectablePage.row2.should("have.class","active");
     })
      // Create scenario 2:
     it.only("Scenario 2", () =>{
       // Click on grid
-      cy.get('#demo-tab-grid').click();
+      SelectablePage.gridButton.click();
       // Click on numbers 4, 8, 6, 2
-      cy.get('#row2 > :nth-child(1)').click();
-      cy.get('#row2 > :nth-child(2)').click();
-      cy.get('#row2 > :nth-child(3)').click();
-      cy.get('#row1 > :nth-child(2)').click();
+      SelectablePage.getFour.click();
+      SelectablePage.getEight.click();
+      SelectablePage.getSix.click();
+      SelectablePage.getTwo.click();
       // Validate that chosen buttons contains class active
-      cy.get('#row2 > :nth-child(1)').should("have.class","active");
-      cy.get('#row2 > :nth-child(2)').should("have.class","active");
-      cy.get('#row2 > :nth-child(3)').should("have.class","active");
-      cy.get('#row1 > :nth-child(2)').should("have.class","active");
+      SelectablePage.getFour.should("have.class","active");
+      SelectablePage.getEight.should("have.class","active");
+      SelectablePage.getSix.should("have.class","active");
+      SelectablePage.getTwo.should("have.class","active");
     })
-   
-   
   });
 });
--- cypress/pageObjects/selectablePage.js
+++ cypress/pageObjects/selectablePage.js
@@ -4,7 +4,27 @@ class SelectablePage extends BasePage {
     static get url() {
         return "/selectable";
       }
-
+    static get row1(){
+        return cy.get('#verticalListContainer > :nth-child(1)');
+    }
+    static get row2(){
+        return cy.get('#verticalListContainer > :nth-child(2)');
+    }
+    static get gridButton(){
+        return cy.get('#demo-tab-grid');
+    }
+    static get getFour(){
+        return cy.get('#row2 > :nth-child(1)');
+    }
+    static get getEight(){
+        return cy.get('#row2 > :nth-child(2)');
+    }
+    static get getSix(){
+        return cy.get('#row2 > :nth-child(3)');
+    }
+    static get getTwo(){
+        return cy.get('#row1 > :nth-child(2)');
+    }
 }
 
 export default SelectablePage;
